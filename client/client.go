fd21e354a1016d9db61362930c0a93e68ae18a59
erko-perko <erci.gruden@gmail.com>
2025-12-16 20:58:15 +0100

    Spremenjeni importi, error report

--- client/client.go
+++ client/client.go
@@ -3,30 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-	"net"
-	"sort"
-	"sync"
+	"flag"
+	"time"
 
 	pb "razpravljalnica/pb" //import pb.go datoteke
 
 	"google.golang.org/grpc"                             //glavna knjižnica za grpc
 	"google.golang.org/protobuf/types/known/emptypb"     //import zaradi google.protobuf.Empty, ki ga uporabimo v metodah, kazalec na "nic"
-	"google.golang.org/protobuf/types/known/timestamppb" //uporabimo zaradi create_at, event_at
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	// preberemo argumente iz ukazne vrstice
 	sPtr := flag.String("s", "", "server URL")
-	pPtr := flag.Int("p", 9876, "port number")
+	pPtr := flag.Int("p", 50051, "port number")
 	flag.Parse()
 
 	// zaženemo strežnik ali odjemalca
 	url := fmt.Sprintf("%v:%v", *sPtr, *pPtr)
 	
+	fmt.Printf("gRPC client connecting to %v\n", url)
 	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Failed to connect to server: %v", err)
+		panic(err)
 	}
 	defer conn.Close()
 
@@ -35,39 +34,42 @@ func main() {
 	defer cancel()
 
 	// vzpostavimo vmesnik gRPC
-	grpcClient := razpravljalnica.NewMessageBoardClient(conn)
+	grpcClient := pb.NewMessageBoardClient(conn)
 
-	user, err := grpcClient.CreateUser(contextCRUD, &razpravljalnica.CreateUserRequest{Username: "janez"})
+	user, err := grpcClient.CreateUser(contextCRUD, &pb.CreateUserRequest{Name: "janez"})
 	if err != nil {
-		log.Fatalf("Could not create user: %v", err)
+		panic(err)
 	}
 
-	topic1, err := grpcClient.CreateTopic(contextCRUD, &razpravljalnica.CreateTopicRequest{Name: "Prva tema"})
+	topic1, err := grpcClient.CreateTopic(contextCRUD, &pb.CreateTopicRequest{Name: "Prva tema"})
 	if err != nil {
-		log.Fatalf("Could not create topic: %v", err)
+		panic(err)
 	}
 
-	topic2, err := grpcClient.CreateTopic(contextCRUD, &razpravljalnica.CreateTopicRequest{Name: "Druga tema"})
+	topic2, err := grpcClient.CreateTopic(contextCRUD, &pb.CreateTopicRequest{Name: "Druga tema"})
 	if err != nil {
-		log.Fatalf("Could not create topic: %v", err)
+		panic(err)
 	}
 
 	topics, err := grpcClient.ListTopics(contextCRUD, &emptypb.Empty{})
 	if err != nil {
-		log.Fatalf("Could not list topics: %v", err)
+		panic(err)
 	}
 	fmt.Println("Topics:")
 	for _, topic := range topics.Topics {
 		fmt.Printf("- %v (ID: %v)\n", topic.Name, topic.Id)
 	}
 
-	subNode, err := grpcClient.GetSubscriptionNode(contextCRUD, &razpravljalnica.SubscriptionNodeRequest{UserId: user.Id, TopicId: []int64{topic1.Id, topic2.Id},})
+	ready := make(chan bool)
+	subNode, err := grpcClient.GetSubscriptionNode(contextCRUD, &pb.SubscriptionNodeRequest{UserId: user.Id, TopicId: []int64{topic1.Id, topic2.Id},})
 	if err != nil {
-		log.Fatalf("Could not get subscription node: %v", err)
+		panic(err)
 	}
+	ready <- true
 
 	go func() {
-		if stream, err := grpcClient.SubscribeTopic(contextCRUD, &razpravljalnica.SubscribeTopicRequest{TopicId: []int64{topic1.Id, topic2.Id}, UserId: user.Id, FromMessageId: 0, SubscribeToken: subNode.SubscribeToken}); err != nil {
+		<-ready
+		if stream, err := grpcClient.SubscribeTopic(contextCRUD, &pb.SubscribeTopicRequest{TopicId: []int64{topic1.Id, topic2.Id}, UserId: user.Id, FromMessageId: 0, SubscribeToken: subNode.SubscribeToken}); err != nil {
 			panic(err)
 		} else {
 			for {
@@ -81,60 +83,60 @@ func main() {
 		}
 	}()
 
-	message1, err := grpcClient.PostMessage(contextCRUD, &razpravljalnica.PostMessageRequest{Text: "Prvo sporočilo v prvi temi", TopicId: topic1.Id, UserId: user.Id,})
+	message1, err := grpcClient.PostMessage(contextCRUD, &pb.PostMessageRequest{Text: "Prvo sporočilo v prvi temi", TopicId: topic1.Id, UserId: user.Id,})
 	if err != nil {
-		log.Fatalf("Could not post message: %v", err)
+		panic(err)
 	}
 
-	message2, err := grpcClient.PostMessage(contextCRUD, &razpravljalnica.PostMessageRequest{Text: "Drugo sporočilo v prvi temi", TopicId: topic1.Id, UserId: user.Id,})
+	message2, err := grpcClient.PostMessage(contextCRUD, &pb.PostMessageRequest{Text: "Drugo sporočilo v prvi temi", TopicId: topic1.Id, UserId: user.Id,})
 	if err != nil {
-		log.Fatalf("Could not post message: %v", err)
+		panic(err)
 	}
 
-	messages, err := grpcClient.GetMessages(contextCRUD, &razpravljalnica.GetMessagesRequest{TopicId: topic1.Id, FromMessageId: 0, Limit: 0,})
+	messages, err := grpcClient.GetMessages(contextCRUD, &pb.GetMessagesRequest{TopicId: topic1.Id, FromMessageId: 0, Limit: 0,})
 	if err != nil {
-		log.Fatalf("Could not get messages: %v", err)
+		panic(err)
 	}
 	fmt.Println("Messages in topic 1:")
 	for _, message := range messages.Messages {
 		fmt.Printf("- %v (ID: %v, Likes: %v)\n", message.Text, message.Id, message.Likes)
 	}
 
-	message3, err := grpcClient.PostMessage(contextCRUD, &razpravljalnica.PostMessageRequest{Text: "Prvo sporočilo v drugi temi", TopicId: topic2.Id, UserId: user.Id,})
+	message3, err := grpcClient.PostMessage(contextCRUD, &pb.PostMessageRequest{Text: "Prvo sporočilo v drugi temi", TopicId: topic2.Id, UserId: user.Id,})
 	if err != nil {
-		log.Fatalf("Could not post message: %v", err)
+		panic(err)
 	}
 
-	message2_1, err := grpcClient.UpdateMessage(contextCRUD, &razpravljalnica.UpdateMessageRequest{TopicId: topic1.Id, UserId: user.Id, MessageId: message2.Id, Text: "Posodobljeno drugo sporočilo v prvi temi",})
+	_, err = grpcClient.UpdateMessage(contextCRUD, &pb.UpdateMessageRequest{TopicId: topic1.Id, UserId: user.Id, MessageId: message2.Id, Text: "Posodobljeno drugo sporočilo v prvi temi",})
 	if err != nil {
-		log.Fatalf("Could not update message: %v", err)
+		panic(err)
 	}
 
-	messages, err := grpcClient.GetMessages(contextCRUD, &razpravljalnica.GetMessagesRequest{TopicId: topic1.Id, FromMessageId: 2, Limit: 0,})
+	messages, err = grpcClient.GetMessages(contextCRUD, &pb.GetMessagesRequest{TopicId: topic1.Id, FromMessageId: 2, Limit: 0,})
 	if err != nil {
-		log.Fatalf("Could not get messages: %v", err)
+		panic(err)
 	}
 	fmt.Println("Messages in topic 1 from message ID 2:")
 	for _, message := range messages.Messages {
 		fmt.Printf("- %v (ID: %v, Likes: %v)\n", message.Text, message.Id, message.Likes)
 	}
 
-	_, err := grpcClient.DeleteMessage(contextCRUD, &razpravljalnica.DeleteMessageRequest{TopicId: topic1.Id, UserId: user.Id, MessageId: message1.Id,})
+	_, err = grpcClient.DeleteMessage(contextCRUD, &pb.DeleteMessageRequest{TopicId: topic1.Id, UserId: user.Id, MessageId: message1.Id,})
 	if err != nil {
-		log.Fatalf("Could not delete message: %v", err)
+		panic(err)
 	}
 
-	messages, err := grpcClient.GetMessages(contextCRUD, &razpravljalnica.GetMessagesRequest{TopicId: topic1.Id, FromMessageId: 0, Limit: 1,})
+	messages, err = grpcClient.GetMessages(contextCRUD, &pb.GetMessagesRequest{TopicId: topic1.Id, FromMessageId: 0, Limit: 1,})
 	if err != nil {
-		log.Fatalf("Could not get messages: %v", err)
+		panic(err)
 	}
 	fmt.Println("Messages in topic 1 with limit 1:")
 	for _, message := range messages.Messages {
 		fmt.Printf("- %v (ID: %v, Likes: %v)\n", message.Text, message.Id, message.Likes)
 	}
 
-	message3_like, err := grpcClient.LikeMessage(contextCRUD, &razpravljalnica.LikeMessageRequest{TopicId: topic2.Id, UserId: user.Id, MessageId: message3.Id,})
+	_, err = grpcClient.LikeMessage(contextCRUD, &pb.LikeMessageRequest{TopicId: topic2.Id, UserId: user.Id, MessageId: message3.Id,})
 	if err != nil {
-		log.Fatalf("Could not like message: %v", err)
+		panic(err)
 	}
 }
